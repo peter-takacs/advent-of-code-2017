6142e608b369b10c8e28708c6cba2ad56c374401
Peter Takacs <hhike@outlook.com>
2018-04-23 15:16:30 +0200

    Added circular copy-paste

--- AdventOfCode/Day10.fs
+++ AdventOfCode/Day10.fs
@@ -1,1 +1,28 @@
 module Day10
+
+open Common
+open System
+
+let extractCircularSublist lst start length =
+    let withoutHead = List.skip start lst;
+    if List.length withoutHead >= length then List.take length withoutHead
+    else
+    List.concat [
+       withoutHead;
+       List.take (length - List.length withoutHead) lst
+    ]
+
+
+let pasteCircular lst start clipboard = 
+    let overflowLength = Math.Max(0, start + (List.length clipboard) - (List.length lst));
+    let clipHead = List.take (List.length clipboard - overflowLength) clipboard;
+    let overflow = List.skip (List.length clipHead) clipboard;
+    List.concat [
+        overflow;
+        List.take start lst |> List.skip overflowLength;
+        clipHead;
+        List.skip (start + List.length clipboard) lst
+    ]
+
+let reverseCircularSublist lst start length = 
+    extractCircularSublist lst start length |> List.rev |> pasteCircular lst start
--- AdventOfCode/Program.fs
+++ AdventOfCode/Program.fs
@@ -8,6 +8,7 @@ open Day6
 open Day7
 open Day8
 open Day9
+open Day10
 open System
 open System.Text.RegularExpressions
 
@@ -15,22 +16,23 @@ open System.Text.RegularExpressions
 
 [<EntryPoint>]
 let main argv = 
-    printfn "Day 1/1: %i" (inverseCaptcha (d1input,  1 ))
-    printfn "Day 1/2: %i" (inverseCaptcha (d1input,  d1input.Length / 2 ))
-    printfn "Day 2/1: %i" (checksum d2input)
-    printfn "Day 2/2: %i" (divisibleChecksum d2input)
-    printfn "Day 3/1: %i" (whereIs 347991 |> manhattanDistance)
-    printfn "Day 3/2: %i" (findFirstLargerThan 347991)
-    printfn "Day 4/1: %i" (d4input |> toTable |> withoutDuplicatesInRows |> Seq.length)
-    printfn "Day 4/2: %i" (d4input |> toTable |> withoutAnagramRows |> Seq.length)
-    printfn "Day 5/1: %i" (Array.copy d5input |> iterateUntilOut (fun i -> i+1))
-    printfn "Day 5/2: %i" (Array.copy d5input |> iterateUntilOut (fun i -> if i>2 then i-1 else i+1))
-    printfn "Day 6/1: %i" <| (Seq.length  <| numberOfStepsUntilLoop d6input)    
-    printfn "Day 6/2: %i" <| (loopLength d6input |> (+) <| 1)
-    printfn "Day 7/1: %s" <| (d7input |> parseLines |> findRoot |> (fun (n: Node) -> n.Name))
-    printfn "Day 7/2: %i" <| (d7input |> parseLines |> getImbalancedWeight)
-    printfn "Day 8/1: %i" <| (d8input |> parse |> getLargestRegister)
-    printfn "Day 8/2: %i" <| (d8input |> parse |> getLargestNumberHeld)
-    printfn "Day 9/1: %i" <| (d9input |> parseGroups |> calculateScore 1)
-    printfn "Day 9/2: %i" <| (d9input |> parseGroups |> calculateGarbageLength)
+    //printfn "Day 1/1: %i" (inverseCaptcha (d1input,  1 ))
+    //printfn "Day 1/2: %i" (inverseCaptcha (d1input,  d1input.Length / 2 ))
+    //printfn "Day 2/1: %i" (checksum d2input)
+    //printfn "Day 2/2: %i" (divisibleChecksum d2input)
+    //printfn "Day 3/1: %i" (whereIs 347991 |> manhattanDistance)
+    //printfn "Day 3/2: %i" (findFirstLargerThan 347991)
+    //printfn "Day 4/1: %i" (d4input |> toTable |> withoutDuplicatesInRows |> Seq.length)
+    //printfn "Day 4/2: %i" (d4input |> toTable |> withoutAnagramRows |> Seq.length)
+    //printfn "Day 5/1: %i" (Array.copy d5input |> iterateUntilOut (fun i -> i+1))
+    //printfn "Day 5/2: %i" (Array.copy d5input |> iterateUntilOut (fun i -> if i>2 then i-1 else i+1))
+    //printfn "Day 6/1: %i" <| (Seq.length  <| numberOfStepsUntilLoop d6input)    
+    //printfn "Day 6/2: %i" <| (loopLength d6input |> (+) <| 1)
+    //printfn "Day 7/1: %s" <| (d7input |> parseLines |> findRoot |> (fun (n: Node) -> n.Name))
+    //printfn "Day 7/2: %i" <| (d7input |> parseLines |> getImbalancedWeight)
+    //printfn "Day 8/1: %i" <| (d8input |> parse |> getLargestRegister)
+    //printfn "Day 8/2: %i" <| (d8input |> parse |> getLargestNumberHeld)
+    //printfn "Day 9/1: %i" <| (d9input |> parseGroups |> calculateScore 1)
+    //printfn "Day 9/2: %i" <| (d9input |> parseGroups |> calculateGarbageLength)
+    printfn "Day 10/1: %A" <| (reverseCircularSublist [1;2;3;4;5] 1 3)
     0
